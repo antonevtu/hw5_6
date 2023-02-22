eebf89a8b958adbe5e8a8acc3c21d56b7b8a36b2
arti <antonevtu@mail.ru>
2023-02-22 23:20:46 +0300

    Done 1

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+use hw5_6::borrowing_house::run_borrowing_provider;
+use hw5_6::owning_house::run_owning_provider;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Owning provider:");
+    run_owning_provider();
+
+    println!("Borrowing provider:");
+    run_borrowing_provider();
 }
